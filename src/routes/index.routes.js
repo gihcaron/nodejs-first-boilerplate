b59392a66548c726d545addf449e44bc4fdedc09
Giovanna Caron <158209996+gihcaron@users.noreply.github.com>
2024-10-24 11:21:20 -0300

    refactor: adjust index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
+// Lista de importação das rotas do projeto
+import usuariosRoutes from "./usuarios.routes";
+
 const routes = Router();
 
 // Rota raiz para teste
 routes.get("/", (req, res) => {
-  return res.status(200).json({ message: "Rota teste funcionando!" });
+  return res.status(200).json({ message: "Rota raiz para teste funcionando!" });
 });
 
-export default routes;
+// Lista de uso das rotas do projeto
+routes.use("/usuarios", usuariosRoutes);
 
+export default routes;
